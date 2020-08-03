51660610b1253d51f20cfda0cb338e5e74f3eca8
Trevor Martin <trevor.j.martin@gmail.com>
2020-08-03 16:02:03 -0400

    first commit

---
+++ src/styles.ts
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  width: 100%;
+  height: 100%;
+  padding: 1.5rem;
+  background-color: #4f84c4;
+`;
+
+export const ColumnContainer = styled.div`
+  background-color: #f1f1f1;
+  width: 300px;
+  min-height: 40px;
+  margin-right: 20px;
+  border-radius: 3px;
+  padding: 1rem;
+  flex-grow: 0;
+`;
+
+export const ColumnTitle = styled.div`
+  padding: 16px 16px 12px;
+  font-weight: bold;
+  background-color: #f1f1f1;
+`;
+
+export const CardContainer = styled.div`
+  background-color: #fff;
+  curser: pointer;
+  margin-bottom: 0.5;
+  padding: 0.5rem 1rem;
+  max-width: 300px;
+  border-radius: 3px;
+  box-shadow: #091e4240 0px 1px 0px 0px;
+`;
+
+interface AddItemButtonProps {
+  dark?: boolean;
+}
+
+export const AddItemButton = styled.button<AddItemButtonProps>`
+  background-color: #ffffff3d;
+  border-radius: 3px;
+  border: none;
+  color: ${(props) => (props.dark ? "#000" : "fff")};
+  cursor: pointer;
+  max-width: 300px;
+  padding: 10px 12px;
+  text-align: left;
+  transition: background 85ms ease-in;
+  width: 100%;
+  &:hover {
+    background-color: #ffffff52;
+  }
+`;
+
+export const NewFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: flex-start;
+  max-width: 300px;
+`;
+
+export const NewItemButton = styled.button`
+  background-color: #5aac55;
+  border-radius: 3px;
+  border: none;
+  box-shadow: none;
+  color: #fff;
+  padding: 6px 12px;
+  text-align: center;
+`;
+
+export const NewItemInput = styled.input`
+  border-radius: 3px;
+  border: none;
+  box-shadow: #091e4240 0px 1px 0px 0px;
+  margin-botton: 0.5rem;
+  padding: 0.5rem 1rem;
+  width: 100%;
+`;
